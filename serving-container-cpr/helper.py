61244fdd60322ff351656461d2ed7c36799d13ac
ritheeshgururi <ritheeshgururi187@gmail.com>
2025-05-31 15:21:00 +0000

    Commit after successful build and push of serving container image, with lightweight version after debugging with manual docker build.

--- serving-container-cpr/helper.py
+++ serving-container-cpr/helper.py
@@ -3,24 +3,21 @@ from src_dir_handler_sdk.handler import CprHandler
 from src_dir_handler_sdk.predictor import CprPredictor
 from google.cloud.aiplatform.prediction import LocalModel
 
-
-
-
 def main():
     
     USER_SRC_DIR = "src_dir_handler_sdk"
 
 
-    BUCKET_URI = "gs://training-data"
-    MODEL_ARTIFACT_DIR = "model_artifacts"
+    # BUCKET_URI = "gs://training-data"
+    # MODEL_ARTIFACT_DIR = "model_artifacts"
 
     PROJECT_ID = "gcp-vertexai-mlops-blueprint"
-    REGION = "us-central1"
-    REPOSITORY = "mlops-repo"
-    IMAGE = "inference-script-image-latest"
+    REGION = "asia-south1"
+    REPOSITORY = "tft-cpr-serving-container"
+    IMAGE = "serving-container-cpr:latest"
 
 
-    MODEL_DISPLAY_NAME = "pytorch-tft-inference"
+    # MODEL_DISPLAY_NAME = "pytorch-tft-inference"
     print("Local Model Build Start")
    
 
@@ -28,8 +25,8 @@ def main():
                                              f"{REGION}-docker.pkg.dev/{PROJECT_ID}/{REPOSITORY}/{IMAGE}",
                                              predictor = CprPredictor,  
                                              handler = CprHandler,  
-                                             requirements_path = os.path.join(USER_SRC_DIR, "requirements.txt"),
-                                             base_image = 'python:3.10',
+                                             requirements_path = os.path.join(USER_SRC_DIR, "cpu_requirements.txt"),
+                                             base_image = 'python:3.11-slim-bookworm',
                                              no_cache = True)
     print("Local Model Build complete")
 
---
+++ serving-container-cpr/src_dir_handler_sdk/handler.py
@@ -0,0 +1,60 @@
+import csv
+from io import StringIO
+import json
+
+from fastapi import Response, Request
+import pandas as pd
+from google.cloud.aiplatform.prediction.handler import PredictionHandler
+
+
+class CprHandler(PredictionHandler):
+    
+    async def handle(self, request: Request):
+        try:
+            request_body = await request.body()
+            print("Data Received")
+            
+            request_data = json.loads(request_body.decode("utf-8"))
+            print(type(request_data["instances"][0]))
+            try:
+                if request_data['request_type'] == "Online":
+                    is_online_prediction = True
+                    data_list = json.loads(request_data["instances"][0])
+                else:
+                    is_online_prediction = False
+                    data_list = request_data["instances"][0]
+            except:
+                is_online_prediction = False
+                data_list = request_data["instances"][0]
+            
+            print("Online Prediction Detected" if is_online_prediction else "Batch Prediction Detected")
+                
+            prediction_instances = pd.DataFrame(request_data["instances"][0])
+            print(prediction_instances.head())
+            print("DataFrame Created")
+
+
+            df = self._predictor.preprocess(data = prediction_instances)
+            print("Preprocess Done")
+            
+            self._predictor.predict()
+            print("Prediction Done")
+            
+            prediction_results = self._predictor.post_process(data = df)
+            print("Postprocess Done")
+            
+            json_res = json.dumps(prediction_results)
+
+            
+            if is_online_prediction:
+                return Response(content=json_res, media_type="application/json")
+            else:
+                json_output = json.dumps({"predictions":[prediction_results]})
+                return Response(content=json_output)
+
+        
+        except Exception as e:
+            message = "Exception : " + str(e)
+            return Response(content = json.dumps({"code" : 500,
+                                                 "Message" : message}))
+        
